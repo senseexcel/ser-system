964ad5bb282df241b559e1c34d8743749049de46
MBerthold <MBerthold@NB-FC-215335>
2020-05-08 14:34:31 +0200

    frist version

---
+++ src/PerfomanceAnalyser.cs
@@ -0,0 +1,69 @@
+﻿namespace Ser.Engine.Diagnostics
+{
+    #region Usings
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    #endregion
+
+    public class PerfomanceAnalyser
+    {
+        #region Properties && Variables
+        private static DateTime starttime;
+        private static readonly object locker = new object();
+        private static readonly List<AnalyserMessage> messages = new List<AnalyserMessage>();
+        public static string FilePath { get; set; }
+        public static char Seperator { get; set; } = '|';
+        #endregion
+
+        #region Public Methods
+        public static void Start()
+        {
+            if (String.IsNullOrEmpty(FilePath))
+                throw new Exception("The perfomance analyser has no filepath.");
+            if (Path.GetExtension(FilePath) != ".perf")
+                throw new Exception("The perfomance analyser file extention must be '.perf'.");
+            starttime = DateTime.Now;
+        }
+
+        public static void Stop()
+        {
+            lock (locker)
+            {
+                using (var csvWriter = new StreamWriter(FilePath, false, Encoding.UTF8))
+                {
+                    csvWriter.WriteLine($"Time{Seperator}Modul{Seperator}StatusNum{Seperator}Message");
+                    foreach (var message in messages)
+                    {
+                        var span = message.Stemp - starttime;
+                        csvWriter.WriteLine($"{span.TotalSeconds}{Seperator}{message.Modulname}{Seperator}{message.StatusNumber}{Seperator}{message.Value}");
+                    }
+                    csvWriter.Flush();
+                }
+            }
+        }
+
+        public static void WriteMessage(int statusNumber, string modulName, string message)
+        {
+            messages.Add(new AnalyserMessage() 
+            { 
+                Stemp = DateTime.Now, 
+                StatusNumber = statusNumber, 
+                Modulname = modulName, 
+                Value = message 
+            });
+        }
+        #endregion
+    }
+
+    #region Helper Class
+    internal class AnalyserMessage
+    {
+        public int StatusNumber { get; set; }
+        public string Modulname { get; set; }
+        public string Value { get; set; }
+        public DateTime Stemp { get; set; }
+    }
+    #endregion
+}
---
+++ src/SystemHealth.cs
@@ -0,0 +1,110 @@
+﻿namespace Ser.Engine.Diagnostics
+{
+    #region Usings
+    using NLog;
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    #endregion
+
+    public class SystemHealth
+    {
+        #region Logger
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        #endregion
+
+        #region Public Methods
+        public static int GetCoreCountForCalc(int taskCoreCount, int taskCount)
+        {
+            try
+            {
+                var systemCoreCount = Environment.ProcessorCount;
+                systemCoreCount--;
+                var result = 1;
+                if (taskCoreCount <= 0)
+                    result = Convert.ToInt32(Math.Ceiling((Convert.ToDouble(systemCoreCount) / Convert.ToDouble(taskCount)))) - 3;
+                if (result <= 0)
+                    result = 1;
+                if (taskCoreCount > 0 && systemCoreCount - 1 > taskCoreCount)
+                    result = taskCoreCount;
+                logger.Info($"The calculation uses {result} CPU cores.");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, ">>>The calculation cores of the System could not be read. The calculation runs with 1 core.<<<");
+                return 1;
+            }
+        }
+
+        public static int GetCoreCountForTask(int taskCoreCount, int taskCount)
+        {
+            try
+            {
+                var systemCoreCount = Environment.ProcessorCount;
+                logger.Debug($"The system has {systemCoreCount} CPU cores.");
+                systemCoreCount--;
+                var result = 1;
+                if (taskCoreCount <= -1)
+                    taskCoreCount = systemCoreCount;
+
+                if (taskCoreCount > 1)
+                {
+                    if (systemCoreCount >= taskCount * 2)
+                        result = taskCount;
+                    else
+                    {
+                        result = Convert.ToInt32(Math.Round(Convert.ToDouble(systemCoreCount) / Convert.ToDouble(taskCount), 0));
+                        if (result <= 1)
+                            result = 1;
+                    }
+                }
+
+                logger.Info($"The engine uses {result} CPU cores.");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, ">>>The CPU cores of the System could not be read. The engine runs with 1 core.<<<");
+                return 1;
+            }
+        }
+
+        public static bool UseMemory(double userMemoryLimit = -1)
+        {
+            try
+            {
+                if (userMemoryLimit == -1)
+                {
+                    logger.Debug("Use the full memory of the system. No memory limit.");
+                    return true;
+                }
+
+                logger.Debug("Memory limit is use.");
+                var engineProc = Process.GetCurrentProcess();
+                double procGbSize = Convert.ToDouble(engineProc?.PrivateMemorySize64) / 1024 / 1024 / 1024;
+                double procent = procGbSize * 100 / userMemoryLimit;
+                logger.Trace($"Use {procent} of memory.");
+                if (procent >= 90)
+                {
+                    logger.Debug($"The memory size for the engine is limited by {userMemoryLimit} GB.");
+                    return false;
+                }
+                logger.Trace("Used memory is O.K.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, ">>>The memory limit could not be calculated.<<<");
+                return true;
+            }
+        }
+
+        public static void ThrowIfCanceledByTimoutorUser(CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+                throw new OperationCanceledException("Task was terminated by timeout or user.");
+        }
+        #endregion
+    }
+}
