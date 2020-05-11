5a1d38b8d6dee3931a140d771e7d17d3dd6bbe5d
MBerthold <MBerthold@NB-FC-215335>
2020-05-11 09:08:35 +0200

    update logic

--- src/PerfomanceAnalyser.cs
+++ src/PerfomanceAnalyser.cs
@@ -2,67 +2,117 @@
 {
     #region Usings
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
+    using System.Reflection;
     using System.Text;
+    using Newtonsoft.Json;
     #endregion
 
     public class PerfomanceAnalyser
     {
         #region Properties && Variables
-        private static DateTime starttime;
-        private static readonly object locker = new object();
-        private static readonly List<AnalyserMessage> messages = new List<AnalyserMessage>();
-        public static string FilePath { get; set; }
-        public static char Seperator { get; set; } = '|';
+        private DateTime starttime;
+        private readonly static object locker = new object();
+        private readonly ConcurrentStack<AnalyserCheckPoint> checkPoints = new ConcurrentStack<AnalyserCheckPoint>();
+        public AnalyserOptions Options { get; private set; }
+        #endregion
+
+        #region Constructor
+        public PerfomanceAnalyser(AnalyserOptions options = null)
+        {
+            if (options == null)
+                options = new AnalyserOptions();
+
+            Options = options;
+        }
         #endregion
 
         #region Public Methods
-        public static void Start()
+        public void Start()
         {
-            if (String.IsNullOrEmpty(FilePath))
-                throw new Exception("The perfomance analyser has no filepath.");
-            if (Path.GetExtension(FilePath) != ".perf")
-                throw new Exception("The perfomance analyser file extention must be '.perf'.");
             starttime = DateTime.Now;
         }
 
-        public static void Stop()
+        public void Stop(string analyserFolder)
         {
-            lock (locker)
+            if (!Directory.Exists(analyserFolder))
+                throw new Exception($"The perfomance analyser folder {analyserFolder} does not exists.");
+
+            var results = new List<AnalyserCheckPoint>();
+            var files = Directory.GetFiles(analyserFolder, "*.perfc", SearchOption.TopDirectoryOnly);
+            foreach (var file in files)
             {
-                using (var csvWriter = new StreamWriter(FilePath, false, Encoding.UTF8))
+                var json = File.ReadAllText(file, Encoding.UTF8);
+                results.AddRange(JsonConvert.DeserializeObject<List<AnalyserCheckPoint>>(json));
+                File.Delete(file);
+            }
+
+            foreach (var item in results)
+                item.SortSpan = (item.Stemp - starttime).TotalSeconds;
+
+            results = results.OrderBy(c => c.SortSpan).ToList();
+
+            var savePath = Path.Combine(analyserFolder, $"analyser.perf");
+            using (var csvWriter = new StreamWriter(savePath, false, Encoding.UTF8))
+            {
+                var headers = new List<string> { "Time", "Modul", "Action", "Message" };
+                csvWriter.WriteLine(String.Join(Options.Seperator, headers));
+                foreach (var checkpoint in results)
                 {
-                    csvWriter.WriteLine($"Time{Seperator}Modul{Seperator}StatusNum{Seperator}Message");
-                    foreach (var message in messages)
-                    {
-                        var span = message.Stemp - starttime;
-                        csvWriter.WriteLine($"{span.TotalSeconds}{Seperator}{message.Modulname}{Seperator}{message.StatusNumber}{Seperator}{message.Value}");
-                    }
-                    csvWriter.Flush();
+                    var rows = new List<string>
+                        {
+                            checkpoint.SortSpan.ToString(),
+                            checkpoint.Modulname,
+                            checkpoint.Action,
+                            checkpoint.Message
+                        };
+                    csvWriter.WriteLine(String.Join(Options.Seperator, rows));
                 }
+                csvWriter.Flush();
             }
         }
 
-        public static void WriteMessage(int statusNumber, string modulName, string message)
+        public void SetCheckPoint(string action, string message)
         {
-            messages.Add(new AnalyserMessage() 
-            { 
-                Stemp = DateTime.Now, 
-                StatusNumber = statusNumber, 
-                Modulname = modulName, 
-                Value = message 
+            checkPoints.Push(new AnalyserCheckPoint()
+            {
+                Stemp = DateTime.Now,
+                Action = action,
+                Message = message
             });
+        }
+
+        public void SaveCheckPoints(string saveFolder, string modulName)
+        {
+            if (String.IsNullOrEmpty(saveFolder))
+                throw new Exception("The perfomance analyser has no folder path.");
+            lock (locker)
+            {
+                foreach (var checkPoint in checkPoints)
+                    checkPoint.Modulname = modulName;
+                var savePath = Path.Combine(saveFolder, $"{modulName}.perfc");
+                var json = JsonConvert.SerializeObject(checkPoints, Formatting.Indented);
+                File.WriteAllText(savePath, json, Encoding.UTF8);
+            }
         }
         #endregion
     }
 
     #region Helper Class
-    internal class AnalyserMessage
+    public class AnalyserOptions
     {
-        public int StatusNumber { get; set; }
+        public char Seperator { get; set; } = '|';
+    }
+
+    internal class AnalyserCheckPoint
+    {
+        internal double SortSpan { get; set; }
+        public string Action { get; set; }
         public string Modulname { get; set; }
-        public string Value { get; set; }
+        public string Message { get; set; }
         public DateTime Stemp { get; set; }
     }
     #endregion
